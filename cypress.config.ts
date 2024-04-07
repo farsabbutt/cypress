9c1f77c9e6052afd85d5d5823b7b1bb3e050009a
Farsab <farsabb@gmail.com>
2024-04-07 05:33:18 +0200

    testing parallel run with coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "reportDir": "cypress/reports/mocha",
+    "overwrite": false,
+    "html": false,
+    "json": true
+  },
   e2e: {
-    specPattern: ['./cypress/e2e/own/*.*'],
+    baseUrl: "https://example.cypress.io",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('@cypress/code-coverage/task')(on, config)
